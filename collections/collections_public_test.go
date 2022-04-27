fe28defdb8e01218f7d6ff6243ce33fe4ee3a2be
Israel Torres <israel.torres@nurx.co>
2022-04-26 20:29:21 -0700

    Split public tests out, group tests into Runs, and missing test for 100% coverage

--- collections/collections_public_test.go
+++ collections/collections_public_test.go
@@ -8,10 +8,21 @@ import (
 )
 
 func TestSortEvens(t *testing.T) {
-	input := []int{2, 8, 7, 1, 3, 5, 6, 4}
-	expected := []int{2, 4, 6, 8}
-	result := collections.SortEvens(input)
-	if diff := cmp.Diff(expected, result); diff != "" {
-		t.Errorf(diff)
-	}
+	t.Run("empty slice", func(t *testing.T) {
+		input := []int{}
+		expected := []int{}
+		result := collections.SortEvens(input)
+		if diff := cmp.Diff(expected, result); diff != "" {
+			t.Errorf(diff)
+		}
+	})
+
+	t.Run("slice of 8 int", func(t *testing.T) {
+		input := []int{2, 8, 7, 1, 3, 5, 6, 4}
+		expected := []int{2, 4, 6, 8}
+		result := collections.SortEvens(input)
+		if diff := cmp.Diff(expected, result); diff != "" {
+			t.Errorf(diff)
+		}
+	})
 }
